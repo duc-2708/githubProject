58ced3b275b1681bca2f0eb847e7c91679f8990d
Ngoc01012 <ngocbede01012006@gmail.com>
2026-04-21 14:47:08 +0700

    hoàn thành hiển thị danh Mục

---
+++ src/main/java/com/example/controller/DanhMucController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DanhMucController {
+public String hienThi() {
+	return"Đây là hiển thị";
+}
+}
