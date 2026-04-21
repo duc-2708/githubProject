8883d70f5ff2129a570ec91f31f2c34b8e08e910
datnguyen098 <hongdat098789@gmail.com>
2026-04-21 14:46:58 +0700

    hoan thanh voucher

---
+++ src/main/java/com/example/controller/VoucherController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VoucherController {
+ public String Voucher() {
+	 return "Voucher";
+ }
+}
