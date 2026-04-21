97652816772dd1a6a2fcc86857a6b84de2895faa
duc-2708 <nguyenductien27082005@gmail.com>
2026-04-21 14:47:00 +0700

    hoàn thành hiển thị SanPham

---
+++ src/main/java/com/example/controller/SanPhamController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SanPhamController {
+	public String show() {
+		return "đây là sản phẩm";
+	}
+}
